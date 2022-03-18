a6f4cfa82f5365cec36ba4794b96c9b3e46b90d0
naufalakmal <muhammadnaufalakmal1@gmail.com>
2022-03-18 15:10:35 +0700

    Resetting Quiz

--- lib/result.dart
+++ lib/result.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText;
     if (resultScore >= 90) {
       resultText =
-          'Score : $resultScore | Mantap! kamu berhasil mendapat nilai yg bagus!';
+          'Score : $resultScore \n Mantap! kamu berhasil mendapat nilai yg bagus!';
     } else if (resultScore >= 75) {
-      resultText = 'Score : $resultScore | Lumayan!';
+      resultText = 'Score : $resultScore \n Lumayan!';
     } else if (resultScore >= 60) {
-      resultText = 'Score : $resultScore | Semangat! Kamu masih bisa coba lagi';
+      resultText =
+          'Score : $resultScore \n Semangat! Kamu masih bisa coba lagi';
     } else {
-      resultText = 'Score : $resultScore | Tolol!';
+      resultText = 'Score : $resultScore \n Tolol!';
     }
     return resultText;
   }
@@ -23,9 +25,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
