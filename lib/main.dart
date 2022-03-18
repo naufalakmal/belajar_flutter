1103dc296dd27469ae037b23e2251bc5e2d4653a
naufalakmal <muhammadnaufalakmal1@gmail.com>
2022-03-18 14:51:12 +0700

    Getters & else if

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText;
+    if (resultScore >= 90) {
+      resultText =
+          'Score : $resultScore | Mantap! kamu berhasil mendapat nilai yg bagus!';
+    } else if (resultScore >= 75) {
+      resultText = 'Score : $resultScore | Lumayan!';
+    } else if (resultScore >= 60) {
+      resultText = 'Score : $resultScore | Semangat! Kamu masih bisa coba lagi';
+    } else {
+      resultText = 'Score : $resultScore | Tolol!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You Did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
